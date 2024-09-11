cb396b9b29dcd64c6d5d0e52ad12a6a2ee5effba
pelippershiny <pelippershiny@gmail.com>
2024-09-11 20:32:46 +0200

    update Launcher with requirements

--- descargaManga.bat
+++ descargaManga.bat
@@ -1,4 +1,10 @@
 @echo off
+REM Verificar si pip está instalado y si no, instalarlo
+python -m ensurepip --default-pip
+
+REM Instalar los módulos necesarios desde requirements.txt
+pip install -r requirements.txt
+
 REM Ejecutar el script get_manga.py y mostrar la salida en la consola
 python get_manga.py
 
